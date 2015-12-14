544763ada6776fb2fc93a5f9ffd962b3c78aff31
Andrew Roth <andrewjlroth@gmail.com>
2015-12-13 18:21:51 -0800

    Updated museq example

--- examples/components/variant_calling/run_museq.py
+++ examples/components/variant_calling/run_museq.py
@@ -1,27 +1,15 @@
 import pypeliner
 
-from biowrappers.components.variant_calling.utils import default_chromosomes
-
+import biowrappers.cli as cli
 import biowrappers.components.variant_calling.museq as museq
 
 def main(args):
-    native_spec = '-V -q all.q -l mem_token={mem}G,mem_free={mem}G,h_vmem={mem}G'
-    
-    config = {
-        'tmpdir' : args.log_dir,
-        'pretend' : False,
-        'submit' : 'asyncqsub',
-        'nativespec' : native_spec,
-        'maxjobs' : 100,
-        'nocleanup' : False
-    }
+    config = cli.load_pypeliner_config(args)
     
     pyp = pypeliner.app.Pypeline([], config)
     
-    scheduler = pyp.sch
-    
-    museq.museq_pipeline(
-        scheduler,
+    workflow = museq.museq_pipeline(
+        args.model_file,
         args.normal_bam_file,
         args.tumour_bam_file,
         args.ref_genome_fasta_file,
@@ -31,28 +19,22 @@ def main(args):
         split_size=args.split_size
     )
     
-    pyp.run()
+    pyp.run(workflow)
 
 if __name__ == '__main__':
     import argparse
     
     parser = argparse.ArgumentParser()
     
-    parser.add_argument('--normal_bam_file', required=True)
+    parser.add_argument('--model_file', required=True)
     
-    parser.add_argument('--tumour_bam_file', required=True)
-    
-    parser.add_argument('--ref_genome_fasta_file', required=True)
+    cli.add_normal_tumour_bam_variant_calling_args(parser)
     
     parser.add_argument('--out_file', required=True)
     
-    parser.add_argument('--install_dir', required=True)
+    cli.add_variant_calling_region_args(parser)
     
-    parser.add_argument('--chromosomes', nargs='+', default=default_chromosomes)
-    
-    parser.add_argument('--split_size', default=int(1e6), type=int)
-    
-    parser.add_argument('--log_dir', default='./')
+    cli.add_pypeliner_args(parser)
     
     args = parser.parse_args()
     
