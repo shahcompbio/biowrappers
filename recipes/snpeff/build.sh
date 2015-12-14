7ad5cd2b6d1f14ce8ad8613f839831db220bcc7c
Andrew Roth <andrewjlroth@gmail.com>
2015-12-13 17:45:43 -0800

    Added snpEff recipe.

---
+++ recipes/snpeff/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
+
+mkdir -p $outdir
+
+mkdir -p $PREFIX/bin
+
+cp -R ./* $outdir/
+
+cp $RECIPE_DIR/snpeff.py $outdir/snpeff.py
+
+chmod +x $outdir/snpeff.py
+
+ln -s $outdir/snpeff.py $PREFIX/bin/snpeff
+
+cd $outdir
+
+snpeff download GRCh37.75
