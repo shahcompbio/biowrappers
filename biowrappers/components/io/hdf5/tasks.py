4737ec8a9933ea1bfe54c54ac0f0bd4cd5bf6d6e
Andrew Roth <andrewjlroth@gmail.com>
2016-02-11 18:13:48 -0800

    ehancement: Changed HDF5 concatenate to use categorical instead of strings to encode non-numerics by default.

--- biowrappers/components/io/hdf5/tasks.py
+++ biowrappers/components/io/hdf5/tasks.py
@@ -8,10 +8,14 @@ import pandas as pd
 
 from biowrappers.components.utils import flatten_input
 
-def concatenate_tables(in_files, out_file):
+def concatenate_tables(in_files, out_file, non_numeric_as_category=True):
     in_files = flatten_input(in_files)
-
-    min_itemsize = _get_min_itemsize(in_files)
+    
+    if non_numeric_as_category:
+        col_categories = _get_column_categories(in_files)
+    
+    else:
+        min_itemsize = _get_min_itemsize(in_files)
     
     out_store = pd.HDFStore(out_file, 'w', complevel=9, complib='blosc')
     
@@ -21,12 +25,19 @@ def concatenate_tables(in_files, out_file):
         for table_name in in_store.keys():
             df = in_store[table_name]
             
-            non_numeric_cols = df.select_dtypes(exclude=[pd.np.number, ]).columns
+            non_numeric_cols = _get_non_numeric_columns(df)
             
-            for col in non_numeric_cols:
-                df[col] = df[col].astype(str)
+            if non_numeric_as_category:
+                for col in non_numeric_cols:
+                    df[col] = df[col].astype('category', categories=col_categories[table_name][col])
+                
+                out_store.append(table_name, df)
             
-            out_store.append(table_name, df, min_itemsize=min_itemsize[table_name])
+            else:
+                for col in non_numeric_cols:
+                    df[col] = df[col].astype(str)
+                
+                out_store.append(table_name, df, min_itemsize=min_itemsize[table_name])
         
         in_store.close()
     
@@ -48,9 +59,7 @@ def _get_min_itemsize(file_list):
             if df.empty:
                 continue
             
-            non_numeric_cols = df.select_dtypes(exclude=[pd.np.number, ]).columns
-            
-            for col in non_numeric_cols:
+            for col in _get_non_numeric_columns(df):
                 df[col] = df[col].astype(str)
                 
                 size = max(8, df[col].str.len().max())
@@ -61,6 +70,40 @@ def _get_min_itemsize(file_list):
         hdf_store.close()
         
     return min_sizes
+
+def _get_column_categories(file_list):
+    '''
+    Find the union set of categories for each column across tables.
+    '''
+  
+    categories = {}
+    
+    for file_name in file_list:
+        hdf_store = pd.HDFStore(file_name, 'r')
+        
+        for table_name in hdf_store.keys():
+            if table_name not in categories:
+                categories[table_name] = {}
+            
+            df = hdf_store[table_name]
+            
+            if df.empty:
+                continue
+            
+            for col in _get_non_numeric_columns(df):
+                df[col] = df[col].astype('category')
+                
+                if col not in categories[table_name]:
+                    categories[table_name][col] = set()
+                
+                categories[table_name][col].update(set(df[col].cat.categories))
+                
+        hdf_store.close()
+        
+    return categories
+
+def _get_non_numeric_columns(df):
+    return df.select_dtypes(exclude=[pd.np.number, ]).columns
 
 def convert_hdf5_to_tsv(in_file, key, out_file, compress=False, index=False):
     df = pd.read_hdf(in_file, key)
