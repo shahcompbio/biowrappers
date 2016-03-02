584a84b962b5c86072fdef028e64e1398a6ee7fa
fongchun <fongchunchan@gmail.com>
2016-03-02 11:27:04 -0800

    Fixing Bug with -rp Parameter

--- biowrappers/cli.py
+++ biowrappers/cli.py
@@ -14,7 +14,7 @@ def add_pypeliner_args(parser):
     
     parser.add_argument('-s', '--submit_method', choices=['asyncqsub', 'drmaa', 'local'], default='local')
 
-    parser.add_argument('-rp', '--repopulate', nargs="?")
+    parser.add_argument('-rp', '--repopulate', action = "store_true")
 
 def add_variant_calling_region_args(parser):
     from biowrappers.components.variant_calling.utils import default_chromosomes
