48d8c65ef889e2b2152ed0b180d5c0b23a647135
Andrew Roth <andrewjlroth@gmail.com>
2016-03-01 18:04:07 -0800

    Added recipes for mutect and strelka.

---
+++ conda/recipes/mutect/build.sh
@@ -0,0 +1,16 @@
+outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
+
+sh $RECIPE_DIR/build_java.sh $outdir
+
+# Copy files
+mkdir -p $outdir
+
+cp mutect-$PKG_VERSION.jar $outdir/mutect.jar
+
+cp $RECIPE_DIR/mutect.py $outdir/mutect.py
+
+chmod +x $outdir/mutect.py
+
+mkdir -p $PREFIX/bin
+
+ln -s $outdir/mutect.py $PREFIX/bin/mutect
