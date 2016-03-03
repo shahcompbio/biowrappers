52b2aecd1e427a619814e2e692f17626751013d6
Andrew McPherson <andrew.mcpherson@gmail.com>
2016-03-02 16:22:44 -0800

    merge results from breakpoint callers

--- biowrappers/pipelines/breakpoint_call_and_annotate/__init__.py
+++ biowrappers/pipelines/breakpoint_call_and_annotate/__init__.py
@@ -3,6 +3,7 @@ from pypeliner.workflow import Workflow
 import os
 import pypeliner  
 
+import biowrappers.components.io.hdf5.tasks as hdf5_tasks
 from biowrappers.components.utils import make_parent_directory
 
 import biowrappers.components.breakpoint_calling.destruct as destruct
@@ -15,18 +16,20 @@ def call_and_annotate_pipeline(
     raw_data_dir,
     results_file,
 ):
-    destruct_raw_data = os.path.join(raw_data_dir, 'destruct')
-    destruct_results_filename = os.path.join(destruct_raw_data, 'results.h5')
-    make_parent_directory(destruct_results_filename)
-
     workflow = Workflow()
     
     workflow.setobj(
         obj=pypeliner.managed.OutputChunks('tumour_sample_id'),
         value=tumour_bam_paths.keys(),
     )
+
+    merge_inputs = {}
     
     if 'destruct' in config:
+        destruct_raw_data = os.path.join(raw_data_dir, 'destruct')
+        destruct_results_filename = os.path.join(destruct_raw_data, 'results.h5')
+        make_parent_directory(destruct_results_filename)
+
         workflow.subworkflow(
             name='destruct',
             func=destruct.destruct_pipeline,
@@ -39,6 +42,18 @@ def call_and_annotate_pipeline(
                 destruct_raw_data,
             ),
         )
+
+        merge_inputs['/breakpoints/destruct'] = pypeliner.managed.InputFile(destruct_results_filename)
+
+    workflow.transform(
+        name='merge_results',
+        ctx={'mem': 8},
+        func=hdf5_tasks.merge_hdf5,
+        args=(
+            merge_inputs,
+            pypeliner.managed.OutputFile(results_file),
+        )
+    )
 
     return workflow
 
