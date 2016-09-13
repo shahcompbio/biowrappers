cb5933cc8f9507b06edf5d71053277365e3388c5
Andrew McPherson <andrew.mcpherson@gmail.com>
2016-09-13 15:07:28 -0700

    germline calling pipeline update

--- biowrappers/components/variant_calling/samtools/__init__.py
+++ biowrappers/components/variant_calling/samtools/__init__.py
@@ -6,8 +6,8 @@ import biowrappers.components.variant_calling.utils as utils
 import biowrappers.components.io.vcf.tasks as vcf_tasks
 import tasks
 
+
 def create_samtools_variant_calling_workflow(
-        sch,
         bam_file,
         ref_genome_fasta_file,
         indel_vcf_file,
@@ -16,47 +16,72 @@ def create_samtools_variant_calling_workflow(
         split_size=int(1e7)
     ):
     
-    sch.setobj(
-        pypeliner.managed.TempOutputObj('regions_obj', 'regions'),
-        utils.get_regions(bam_file, split_size, chromosomes=chromosomes)
+    workflow = Workflow()
+    
+    workflow.setobj(
+        obj=pypeliner.managed.TempOutputObj('regions_obj', 'regions'),
+        value=utils.get_regions(bam_file, split_size, chromosomes=chromosomes)
     )
     
-    sch.transform(
-        'run_samtools_variant_calling',
-        ('regions',),
-        {'mem' : 4},
-        tasks.run_samtools_variant_calling,
-        None,
-        pypeliner.managed.InputFile(bam_file),
-        pypeliner.managed.InputFile(ref_genome_fasta_file),
-        pypeliner.managed.TempOutputFile('variants.vcf', 'regions'),
-        region=pypeliner.managed.TempInputObj('regions_obj', 'regions')
+    workflow.transform(
+        name='run_samtools_variant_calling',
+        axes=('regions',),
+        ctx={'mem' : 4},
+        func=tasks.run_samtools_variant_calling,
+        args=(
+            pypeliner.managed.InputFile(bam_file),
+            pypeliner.managed.InputFile(ref_genome_fasta_file),
+            pypeliner.managed.TempOutputFile('variants.vcf', 'regions'),
+        ),
+        kwargs={
+            'region': pypeliner.managed.TempInputObj('regions_obj', 'regions'),
+        },
     )
     
-    sch.transform(
-        'merge_indels',
-        (),
-        {'mem' : 2},
-        vcf_tasks.concatenate_vcf,
-        None,
-        pypeliner.managed.TempInputFile('variants.vcf', 'regions'),
-        pypeliner.managed.TempOutputFile('indels.vcf'),
-        variant_filter='indel'
+    workflow.transform(
+        name='merge_indels',
+        ctx={'mem' : 2},
+        func=vcf_tasks.concatenate_vcf,
+        args=(
+            pypeliner.managed.TempInputFile('variants.vcf', 'regions'),
+            pypeliner.managed.TempOutputFile('indels.vcf'),
+        ),
+        kwargs={
+            'variant_filter': 'indel',
+        },
     )
     
-    sch.transform(
-        'merge_snvs',
-        (),
-        {'mem' : 2},
-        vcf_tasks.concatenate_vcf,
-        None,
-        pypeliner.managed.TempInputFile('variants.vcf', 'regions'),
-        pypeliner.managed.TempOutputFile('snvs.vcf'),
-        variant_filter='snv'
+    workflow.transform(
+        name='merge_snvs',
+        ctx={'mem' : 2},
+        func=vcf_tasks.concatenate_vcf,
+        args=(
+            pypeliner.managed.TempInputFile('variants.vcf', 'regions'),
+            pypeliner.managed.TempOutputFile('snvs.vcf'),
+        ),
+        kwargs={
+            'variant_filter': 'snv',
+        },
     )
     
-    vcf_tasks.finalise_vcf(sch, 'indel', 'indels.vcf', indel_vcf_file)
+    workflow.subworkflow(
+        name='finalize_snvs',
+        func=vcf_tasks.finalise_vcf,
+        args=(
+            pypeliner.managed.TempOutputFile('snvs.vcf'),
+            pypeliner.managed.OutputFile(snv_vcf_file),
+        )
+    )
     
-    vcf_tasks.finalise_vcf(sch, 'snv', 'snvs.vcf', snv_vcf_file)
+    workflow.subworkflow(
+        name='finalize_indels',
+        func=vcf_tasks.finalise_vcf,
+        args=(
+            pypeliner.managed.TempOutputFile('indels.vcf'),
+            pypeliner.managed.OutputFile(snv_vcf_file),
+        )
+    )
+    
+    return workflow
     
     
