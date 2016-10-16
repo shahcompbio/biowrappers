872c09bae93699647872e75cbcbcf6ef0b5a5cfc
Andrew Roth <andrewjlroth@gmail.com>
2016-10-16 14:57:30 +0100

    updated picard wrapper

--- biowrappers/components/ngs/picard/tasks.py
+++ biowrappers/components/ngs/picard/tasks.py
@@ -1,16 +1,41 @@
 from biowrappers.components.utils import flatten_input
 
+import os
 import pypeliner
 
 
-def merge(in_files, out_file):
+def merge(in_files, out_file, index_file=None):
+    os.environ['MALLOC_ARENA_MAX'] = '4'
     cmd = [
         'picard',
+        '-XX:ParallelGCThreads=1',
+        '-Xmx8g',
         'MergeSamFiles',
-        'SO=coordinate',
-        'USE_THREADING=false',
+        'OUTPUT={0}'.format(out_file),
         'VALIDATION_STRINGENCY=LENIENT',
-        'OUTPUT={}'.format(out_file),
     ]
-    cmd.extend(['INPUT={}'.format(x) for x in flatten_input(in_files)])
+    for file_name in flatten_input(in_files):
+        cmd.append('INPUT={0}'.format(file_name))
+    pypeliner.commandline.execute(*cmd)
+    if index_file is not None:
+        cmd = ['samtools', 'index', out_file, index_file]
+    pypeliner.commandline.execute(*cmd)
+
+
+def mark_duplicates(in_files, out_file, metrics_file, index_file=None):
+    os.environ['MALLOC_ARENA_MAX'] = '4'
+    cmd = [
+        'picard',
+        '-XX:ParallelGCThreads=1',
+        '-Xmx8g',
+        'MarkDuplicates',
+        'OUTPUT={0}'.format(out_file),
+        'METRICS_FILE={0}'.format(metrics_file),
+        'VALIDATION_STRINGENCY=LENIENT',
+    ]
+    for file_name in flatten_input(in_files):
+        cmd.append('INPUT={0}'.format(file_name))
+    pypeliner.commandline.execute(*cmd)
+    if index_file is not None:
+        cmd = ['samtools', 'index', out_file, index_file]
     pypeliner.commandline.execute(*cmd)
