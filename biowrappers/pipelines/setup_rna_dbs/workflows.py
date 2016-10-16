8d66db47825d7199e84033f9aab01d2037be2f4b
Andrew Roth <andrewjlroth@gmail.com>
2016-10-16 17:20:42 +0100

    bugfix: pypeliner clash when subworkflow and temp file have same name

--- biowrappers/pipelines/setup_rna_dbs/workflows.py
+++ biowrappers/pipelines/setup_rna_dbs/workflows.py
@@ -34,7 +34,7 @@ def download_external_files(config):
         axes=('files',),
         args=(
             mgd.TempInputObj('url', 'files'),
-            mgd.TempOutputFile('download', 'files'),
+            mgd.TempOutputFile('download.file', 'files'),
         ),
     )
     workflow.transform(
@@ -42,7 +42,7 @@ def download_external_files(config):
         axes=('files',),
         func=tasks.unzip,
         args=(
-            mgd.TempInputFile('download', 'files'),
+            mgd.TempInputFile('download.file', 'files'),
             mgd.OutputFile('unzipped', 'files', fnames=downloaded_files),
         ),
     )
