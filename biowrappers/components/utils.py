3117c0f94cf076b87298d59ab60a699e709e4eb3
Andrew Roth <andrewjlroth@gmail.com>
2016-10-17 09:44:05 +0100

    fixed flatten to handle single files.

--- biowrappers/components/utils.py
+++ biowrappers/components/utils.py
@@ -8,72 +8,75 @@ import random
 import time
 import errno
 
+
 def get_ancestor_directory(path, level=1):
     '''
     Get the path of the directory a specified number of levels above the given path.
-    
+
     >>> get_ancestor_directory('/foo/bar/some/where/my_file.txt', level=2)
     '/foo/bar/some'
     '''
     ancestor_dir = path
-    
+
     for _ in range(level):
         ancestor_dir = os.path.dirname(ancestor_dir)
-    
+
     return ancestor_dir
+
 
 def make_directory(target_dir, mode=0775):
     '''
     Check if a directory exists and make it if not.
-    
+
     For example, given /some/where make the folder /some/where. If /some does not exist, it will also be made.
-    '''    
+    '''
     i = 0
-    
+
     try:
         old_umask = os.umask(0000)
-        
+
         while not os.path.exists(target_dir):
-            # Randomly sleep for a short random time so multiple simultaneous calls don't try to create the directory. 
+            # Randomly sleep for a short random time so multiple simultaneous calls don't try to create the directory.
             time.sleep(random.random() * 2)
-            
+
             try:
                 os.makedirs(target_dir, mode)
-            
+
             except OSError:
                 i += 1
-                
+
                 if i > 10:
                     raise
-            
+
     finally:
         os.umask(old_umask)
+
 
 def make_parent_directory(file_name, mode=0775):
     '''
     Given a file name, make the parent directory if it does not exist using make_directory.
-    
+
     For example, given /some/where/foo.bar make the folder /some/where.
     '''
     parent_dir = os.path.dirname(file_name)
-    
+
     make_directory(parent_dir, mode=mode)
+
 
 def flatten_input(files):
     if type(files) == dict:
         parsed_files = [files[x] for x in sorted(files)]
-    
+    elif type(files) == str:
+        parsed_files = files
     else:
         parsed_files = []
-    
         for x in files:
             if type(x) == dict:
                 parsed_files.extend([x[y] for y in sorted(x)])
-            
             else:
                 parsed_files.append(x)
-    
     return parsed_files
+
 
 def remove(filename):
     '''
@@ -84,6 +87,7 @@ def remove(filename):
     except OSError as e:
         if e.errno != errno.ENOENT:
             raise
+
 
 def symlink(filename, link_name=None, link_directory=None):
     '''
@@ -109,5 +113,5 @@ def symlink(filename, link_name=None, link_directory=None):
 
 if __name__ == '__main__':
     import doctest
-     
+
     doctest.testmod()
