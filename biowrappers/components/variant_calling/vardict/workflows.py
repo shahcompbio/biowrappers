dcb4cce3426153f331a8b4cf7162a1b41591e6c0
Andrew Roth <andrewjlroth@gmail.com>
2016-10-20 17:35:07 +0100

    refactor: updated single sample vardict workflow to have one output

--- biowrappers/components/variant_calling/vardict/workflows.py
+++ biowrappers/components/variant_calling/vardict/workflows.py
@@ -12,20 +12,17 @@ import tasks
 def create_vardict_single_sample_workflow(
         bam_file,
         ref_genome_fasta_file,
-        indel_vcf_file,
-        snv_vcf_file,
+        out_file,
         chromosomes=default_chromosomes,
         min_allele_frequency=0.01,
         sample_name=None,
         split_size=int(1e7)):
 
     workflow = Workflow()
-
     workflow.setobj(
         obj=pypeliner.managed.TempOutputObj('config', 'regions'),
         value=utils.get_bam_regions(bam_file, split_size, chromosomes=chromosomes)
     )
-
     workflow.transform(
         name='run_vardict',
         axes=('regions',),
@@ -42,64 +39,30 @@ def create_vardict_single_sample_workflow(
             'sample_name': sample_name,
         },
     )
-
+    workflow.transform(
+        name='compress_tmp',
+        axes=('regions',),
+        ctx={'mem': 2, 'num_retry': 3, 'mem_retry_increment': 2},
+        func=vcf_tasks.compress_vcf,
+        args=(
+            pypeliner.managed.TempInputFile('result.vcf', 'regions'),
+            pypeliner.managed.TempOutputFile('result.vcf.gz', 'regions'),
+        ),
+        kwargs={
+            'index_file': pypeliner.managed.TempOutputFile('result.vcf.gz.tbi', 'regions'),
+        }
+    )
     workflow.transform(
         name='concatenate_vcf',
         ctx={'mem': 2, 'num_retry': 3, 'mem_retry_increment': 2},
         func=vcf_tasks.concatenate_vcf,
         args=(
-            pypeliner.managed.TempInputFile('result.vcf', 'regions'),
-            pypeliner.managed.TempOutputFile('result.merged.vcf')
-        )
+            pypeliner.managed.TempInputFile('result.vcf.gz', 'regions'),
+            pypeliner.managed.OutputFile(out_file),
+        ),
+        kwargs={
+            'bcf_index_file': pypeliner.managed.OutputFile(out_file + '.csi'),
+            'vcf_index_file': pypeliner.managed.OutputFile(out_file + '.tbi'),
+        },
     )
-
-    workflow.transform(
-        name='finalise_all_variants',
-        func=vcf_tasks.finalise_vcf,
-        args=(
-            pypeliner.managed.TempInputFile('result.merged.vcf'),
-            pypeliner.managed.TempOutputFile('result.merged.vcf.gz')
-        )
-    )
-
-    workflow.transform(
-        name='filter_indels',
-        ctx={'mem': 2, 'num_retry': 3, 'mem_retry_increment': 2},
-        func=tasks.filter_vcf,
-        args=(
-            pypeliner.managed.TempInputFile('result.merged.vcf.gz'),
-            pypeliner.managed.TempOutputFile('indels.vcf'),
-            'indel'
-        )
-    )
-
-    workflow.transform(
-        name='finalise_indels',
-        func=vcf_tasks.finalise_vcf,
-        args=(
-            pypeliner.managed.TempInputFile('indels.vcf'),
-            pypeliner.managed.OutputFile(indel_vcf_file)
-        )
-    )
-
-    workflow.transform(
-        name='filter_snvs',
-        ctx={'mem': 2, 'num_retry': 3, 'mem_retry_increment': 2},
-        func=tasks.filter_vcf,
-        args=(
-            pypeliner.managed.TempInputFile('result.merged.vcf.gz'),
-            pypeliner.managed.TempOutputFile('snvs.vcf'),
-            'snv'
-        )
-    )
-
-    workflow.transform(
-        name='finalise_snvs',
-        func=vcf_tasks.finalise_vcf,
-        args=(
-            pypeliner.managed.TempInputFile('snvs.vcf'),
-            pypeliner.managed.OutputFile(snv_vcf_file)
-        )
-    )
-
     return workflow
