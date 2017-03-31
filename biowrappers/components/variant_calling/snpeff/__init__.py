3e1a3d06fa26adfdb00b60010f9c70b38905ab25
Andrew Roth <andrewjlroth@gmail.com>
2017-03-31 14:35:51 +0100

    added vcf output option for strelka

--- biowrappers/components/variant_calling/snpeff/__init__.py
+++ biowrappers/components/variant_calling/snpeff/__init__.py
@@ -1,6 +1,7 @@
 from pypeliner.workflow import Workflow
 
 import pypeliner
+import pypeliner.managed as mgd
 
 import biowrappers.components.io.hdf5.tasks as hdf5_tasks
 import biowrappers.components.io.vcf.tasks as vcf_tasks
@@ -12,7 +13,8 @@ def create_snpeff_annotation_workflow(
         target_vcf_file,
         out_file,
         split_size=int(1e3),
-        table_name='snpeff'):
+        table_name='snpeff',
+        vcf_output=False):
 
     workflow = Workflow()
 
@@ -21,8 +23,8 @@ def create_snpeff_annotation_workflow(
         ctx={'mem': 2, 'num_retry': 3, 'mem_retry_increment': 2},
         func=vcf_tasks.split_vcf,
         args=(
-            pypeliner.managed.InputFile(target_vcf_file),
-            pypeliner.managed.TempOutputFile('split.vcf', 'split')
+            mgd.InputFile(target_vcf_file),
+            mgd.TempOutputFile('split.vcf', 'split')
         ),
         kwargs={'lines_per_file': split_size}
     )
@@ -34,31 +36,48 @@ def create_snpeff_annotation_workflow(
         func=tasks.run_snpeff,
         args=(
             db,
-            pypeliner.managed.TempInputFile('split.vcf', 'split'),
-            pypeliner.managed.TempOutputFile('snpeff.vcf', 'split')
+            mgd.TempInputFile('split.vcf', 'split'),
+            mgd.TempOutputFile('snpeff.vcf', 'split')
         )
     )
 
-    workflow.transform(
-        name='convert_vcf_to_table',
-        axes=('split',),
-        ctx={'mem': 4, 'num_retry': 3, 'mem_retry_increment': 2},
-        func=tasks.convert_vcf_to_table,
-        args=(
-            pypeliner.managed.TempInputFile('snpeff.vcf', 'split'),
-            pypeliner.managed.TempOutputFile('snpeff.h5', 'split'),
-            table_name
+    if vcf_output:
+        workflow.transform(
+            name='merge_vcf',
+            axes=(),
+            ctx={'mem': 4, 'num_retry': 3, 'mem_retry_increment': 2},
+            func=vcf_tasks.concatenate_vcf,
+            args=(
+                mgd.TempInputFile('snpeff.vcf', 'split'),
+                mgd.OutputFile(out_file),
+            ),
+            kwargs={
+                'bcf_index_file': mgd.TempOutputFile('bcf.index'),
+                'vcf_index_file': mgd.OutputFile(out_file + '.tbi'),
+            }
         )
-    )
 
-    workflow.transform(
-        name='concatenate_tables',
-        ctx={'mem': 4, 'num_retry': 3, 'mem_retry_increment': 2},
-        func=hdf5_tasks.concatenate_tables,
-        args=(
-            pypeliner.managed.TempInputFile('snpeff.h5', 'split'),
-            pypeliner.managed.OutputFile(out_file)
+    else:
+        workflow.transform(
+            name='convert_vcf_to_table',
+            axes=('split',),
+            ctx={'mem': 4, 'num_retry': 3, 'mem_retry_increment': 2},
+            func=tasks.convert_vcf_to_table,
+            args=(
+                mgd.TempInputFile('snpeff.vcf', 'split'),
+                mgd.TempOutputFile('snpeff.h5', 'split'),
+                table_name
+            )
         )
-    )
+
+        workflow.transform(
+            name='concatenate_tables',
+            ctx={'mem': 4, 'num_retry': 3, 'mem_retry_increment': 2},
+            func=hdf5_tasks.concatenate_tables,
+            args=(
+                mgd.TempInputFile('snpeff.h5', 'split'),
+                mgd.OutputFile(out_file)
+            )
+        )
 
     return workflow
