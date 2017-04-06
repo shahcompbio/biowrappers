89340914bb662e85587c4d0d94527e88e564a566
Andrew Roth <andrewjlroth@gmail.com>
2017-04-06 17:38:08 +0100

    formatting

--- biowrappers/components/ngs/picard/tasks.py
+++ biowrappers/components/ngs/picard/tasks.py
@@ -6,6 +6,7 @@ import pypeliner
 
 def merge(in_files, out_file, index_file=None):
     os.environ['MALLOC_ARENA_MAX'] = '4'
+
     cmd = [
         'picard',
         '-XX:ParallelGCThreads=1',
@@ -14,16 +15,21 @@ def merge(in_files, out_file, index_file=None):
         'OUTPUT={0}'.format(out_file),
         'VALIDATION_STRINGENCY=LENIENT',
     ]
+
     for file_name in flatten_input(in_files):
         cmd.append('INPUT={0}'.format(file_name))
+
     pypeliner.commandline.execute(*cmd)
+
     if index_file is not None:
         cmd = ['samtools', 'index', out_file, index_file]
+
     pypeliner.commandline.execute(*cmd)
 
 
 def mark_duplicates(in_files, out_file, metrics_file, index_file=None):
     os.environ['MALLOC_ARENA_MAX'] = '4'
+
     cmd = [
         'picard',
         '-XX:ParallelGCThreads=1',
@@ -33,9 +39,13 @@ def mark_duplicates(in_files, out_file, metrics_file, index_file=None):
         'METRICS_FILE={0}'.format(metrics_file),
         'VALIDATION_STRINGENCY=LENIENT',
     ]
+
     for file_name in flatten_input(in_files):
         cmd.append('INPUT={0}'.format(file_name))
+
     pypeliner.commandline.execute(*cmd)
+
     if index_file is not None:
         cmd = ['samtools', 'index', out_file, index_file]
+
     pypeliner.commandline.execute(*cmd)
