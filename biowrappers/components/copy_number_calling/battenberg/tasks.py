c0515e682d5987cdb2ad3e6c2b2300d453d26313
Andrew McPherson <andrew.mcpherson@gmail.com>
2017-04-25 15:36:56 -0700

    finished battenberg apart from config work

--- biowrappers/components/copy_number_calling/battenberg/tasks.py
+++ biowrappers/components/copy_number_calling/battenberg/tasks.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import tarfile
 import numpy as np
 import pandas as pd
 
@@ -16,8 +17,16 @@ from biowrappers.components.copy_number_calling.common.tasks import calculate_br
 from biowrappers.components.copy_number_calling.common.utils import calculate_allele_counts
 
 
-thousand_genomes_loci = '{ref_data_dir}/data/ref_db/battenberg/1000genomesloci/1000genomesloci2012_chr{chromosome}.txt'
+#thousand_genomes_loci_directory = '{ref_data_dir}/data/ref_db/battenberg/1000genomesloci/1000genomesloci2012_chr{chromosome}.txt'
 thousand_genomes_snps =  '{ref_data_dir}/data/ref_db/remixt/thousand_genomes_snps.tsv'
+genome_fasta_index = '`pwd`/data/ref_db/GRCh37-lite.fa.fai'
+impute_info_filename = '`pwd`/data/ref_db/battenberg/impute/impute_info.txt'
+thousand_genomes_loci_directory = '`pwd`/data/ref_db/battenberg/1000genomesloci/'
+ignore_contigs_filename = ' `pwd`/data/ref_db/battenberg/ignore_contigs.txt'
+num_threads = '1'
+prob_loci_filename =   /shahlab/amcpherson/anaconda/envs/ith3/lib/perl5/site_perl/5.22.0/auto/share/module/Sanger-CGP-Battenberg-Implement/battenberg/probloci.txt.gz
+assembly =  hg19/1000genomes
+species = '  "Homo sapiens"'
 
 
 def prepare_battenberg_allele_counts(
@@ -99,17 +108,75 @@ def prepare_data(
         chromosomes,
         chromosome_ids)
 
-    
-
+    with tarfile.open(allele_counts_filename, "w:gz") as tar:
+        for filename in itertools.chain(normal_alleles_filenames, tumour_alleles_filenames):
+            tar.add(filename)
 
 
 def run_battenberg(
     allele_counts_filename,
     normal_id,
-    sample_id,
+    tumour_id,
     results_filename,
     temp_directory,
     config):
     """ Run the battenberg method.
     """
-    
+    genome_fasta_index = config['genome_fasta_index']
+    impute_info_filename = config['impute_info_filename']
+    thousand_genomes_loci_directory = config['thousand_genomes_loci_directory']
+    ignore_contigs_filename = config['ignore_contigs_filename']
+    num_threads = config['num_threads']
+    prob_loci_filename = config['prob_loci_filename']
+    assembly = config['assembly']
+    species = config['species']
+
+    pypeliner.commandline.execute(
+        'battenberg.pl',
+        '-outdir', temp_directory,
+        '-reference', genome_fasta_index,
+        '-allele-counts', allele_counts_filename,
+        '-gender', 'XX',
+        '-impute-info', impute_info_filename,
+        '-thousand-genomes-loc',  thousand_genomes_loci_directory,
+        '-ignore-contigs-file', ignore_contigs_filename,
+        '-t', str(num_threads),
+        '-prob-loci', prob_loci_filename,
+        '-tumbam', tumour_id,
+        '-normbam', normal_id
+        '-ra', assembly,
+        '-rs', species)
+
+    cellularity_ploidy_filename = os.path.join(temp_directory, '{}_cellularity_ploidy.txt'.format(tumour_id))
+    cellularity_ploidy = pd.read_csv(cellularity_ploidy_filename, sep='\t')
+    tumour_content = cellularity_ploidy['cellularity'].iloc[0]
+
+    cn_vcf_filename = os.path.join(temp_directory, '{}_cellularity_ploidy.txt'.format(tumour_id))
+    cn_data = []
+    vcf_reader = vcf.Reader(gzip.open(cn_vcf_filename))
+    for vcf_record in vcf_reader:
+        segment_info = {}
+
+        segment_info['chromosome'] = vcf_record.CHROM
+        segment_info['start'] = vcf_record.POS
+        segment_info['end'] = vcf_record.INFO['END']
+
+        for sample in vcf_record.samples:
+            if sample.sample == 'TUMOUR':
+                segment_info['total_1'] = sample.data.TCN
+                segment_info['minor_1'] = sample.data.MCN
+                segment_info['major_1'] = segment_info['total_1'] - segment_info['minor_1']
+                segment_info['fraction_1'] = sample.data.FCF
+
+                segment_info['total_2'] = sample.data.TCS
+                segment_info['minor_2'] = sample.data.MCS
+                segment_info['major_2'] = segment_info['total_2'] - segment_info['minor_2']
+                segment_info['fraction_2'] = sample.data.FCS
+
+        cn_data.append(segment_info)
+
+    cn_data = pd.DataFrame(cn_data)
+
+    with pd.HDFStore(results_filename, 'w') as store:
+        store['cn'] = cn_data
+        store['mix'] = pd.Series([1. - tumour_content, tumour_content])
