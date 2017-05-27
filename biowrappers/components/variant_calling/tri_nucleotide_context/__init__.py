0b93853e423c3e8ba64b31e93baba20b9a2301fa
Andrew Roth <andrewjlroth@gmail.com>
2017-05-27 18:37:47 +0100

    tri nuc support tsv output

--- biowrappers/components/variant_calling/tri_nucleotide_context/__init__.py
+++ biowrappers/components/variant_calling/tri_nucleotide_context/__init__.py
@@ -1,6 +1,5 @@
-from pypeliner.workflow import Workflow
-
 import pypeliner
+import pypeliner.managed as mgd
 
 import biowrappers.components.io.hdf5.tasks as hdf5_tasks
 import biowrappers.components.io.vcf.tasks as vcf_tasks
@@ -11,18 +10,25 @@ def create_vcf_tric_nucleotide_annotation_workflow(
         ref_genome_fasta_file,
         vcf_file,
         out_file,
+        hdf5_output=True,
         split_size=int(1e4),
         table_name='tri_nucleotide_context'):
 
-    workflow = Workflow()
+    if hdf5_output:
+        merged_file = mgd.OutputFile(out_file)
+
+    else:
+        merged_file = mgd.TempFile('merged.h5')
+
+    workflow = pypeliner.workflow.Workflow()
 
     workflow.transform(
         name='split_vcf',
         ctx={'mem': 2, 'num_retry': 3, 'mem_retry_increment': 2},
         func=vcf_tasks.split_vcf,
         args=(
-            pypeliner.managed.InputFile(vcf_file),
-            pypeliner.managed.TempOutputFile('split.vcf', 'split')
+            mgd.InputFile(vcf_file),
+            mgd.TempOutputFile('split.vcf', 'split')
         ),
         kwargs={'lines_per_file': split_size}
     )
@@ -33,9 +39,9 @@ def create_vcf_tric_nucleotide_annotation_workflow(
         ctx={'mem': 4, 'num_retry': 3, 'mem_retry_increment': 2},
         func=tasks.get_tri_nucelotide_context,
         args=(
-            pypeliner.managed.InputFile(ref_genome_fasta_file),
-            pypeliner.managed.TempInputFile('split.vcf', 'split'),
-            pypeliner.managed.TempOutputFile('tri_nucleotide_context.h5', 'split'),
+            mgd.InputFile(ref_genome_fasta_file),
+            mgd.TempInputFile('split.vcf', 'split'),
+            mgd.TempOutputFile('tri_nucleotide_context.h5', 'split'),
             table_name
         )
     )
@@ -45,9 +51,24 @@ def create_vcf_tric_nucleotide_annotation_workflow(
         ctx={'mem': 2, 'num_retry': 3, 'mem_retry_increment': 2},
         func=hdf5_tasks.concatenate_tables,
         args=(
-            pypeliner.managed.TempInputFile('tri_nucleotide_context.h5', 'split'),
-            pypeliner.managed.OutputFile(out_file)
+            mgd.TempInputFile('tri_nucleotide_context.h5', 'split'),
+            merged_file.as_output()
         )
     )
+
+    if not hdf5_output:
+        workflow.transform(
+            name='convert_to_tsv',
+            ctx={'mem': 2, 'num_retry': 3, 'mem_retry_increment': 2},
+            func=hdf5_tasks.convert_hdf5_to_tsv,
+            args=(
+                merged_file.as_input(),
+                table_name,
+                mgd.OutputFile(out_file),
+            ),
+            kwargs={
+                'compress': True,
+            }
+        )
 
     return workflow
