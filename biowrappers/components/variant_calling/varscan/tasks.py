28ef78e22211a49c415970a0461094922898d99a
Andrew Roth <andrewjlroth@gmail.com>
2017-05-27 13:50:29 +0100

    filtering varscan somatic results

--- biowrappers/components/variant_calling/varscan/tasks.py
+++ biowrappers/components/variant_calling/varscan/tasks.py
@@ -6,8 +6,26 @@ Created on 8 Apr 2017
 import os
 import pypeliner.commandline as cli
 import shutil
+import vcf
 
 from biowrappers.components.ngs.samtools.tasks import mpileup
+from biowrappers.components.io.vcf.tasks import index_vcf
+
+
+def filter_somatic_variants(in_file, out_file):
+    reader = vcf.Reader(filename=in_file)
+
+    with open(out_file, 'wb') as out_fh:
+        writer = vcf.Writer(out_fh, reader)
+
+        for record in reader:
+            if (record.FILTER is None) or (len(record.FILTER) == 0):
+                pass_filter = True
+
+            if pass_filter and ('SOMATIC' in record.INFO):
+                writer.write_record(record)
+
+        writer.close()
 
 
 def run_pileup2snp(in_file, out_file):
@@ -55,6 +73,10 @@ def run_somatic(normal_bam, tumour_bam, ref_genome_fasta_file, out_file, region,
 
     indel_file = tmp_prefix + '.indel.vcf'
 
+    concat_file = tmp_prefix + '.concat.vcf.gz'
+
+    filtered_file = tmp_prefix + '.filtered.vcf'
+
     cli.execute('bgzip', snp_file)
 
     cli.execute('bgzip', indel_file)
@@ -67,6 +89,16 @@ def run_somatic(normal_bam, tumour_bam, ref_genome_fasta_file, out_file, region,
 
     cli.execute('tabix', indel_file)
 
-    cli.execute('bcftools', 'concat', snp_file, indel_file, '-a', '-o', out_file, '-O', 'z')
+    cli.execute('bcftools', 'concat', snp_file, indel_file, '-a', '-o', concat_file, '-O', 'z')
+
+    filter_somatic_variants(concat_file, filtered_file)
+
+    cli.execute('bgzip', filtered_file)
+
+    filtered_file = filtered_file + '.gz'
+
+    index_vcf(filtered_file)
+
+    shutil.move(filtered_file, out_file)
 
     shutil.rmtree(tmp_dir)
