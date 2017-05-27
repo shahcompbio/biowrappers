22c225ff136e4ec7cdf3b75d5bb6f78eaa4cf52a
Andrew Roth <andrewjlroth@gmail.com>
2017-05-27 18:50:32 +0100

    hdf5_output kwarg for anno db workflow

--- biowrappers/components/variant_calling/annotated_db_status/__init__.py
+++ biowrappers/components/variant_calling/annotated_db_status/__init__.py
@@ -1,6 +1,5 @@
-from pypeliner.workflow import Workflow
-
 import pypeliner
+import pypeliner.managed as mgd
 
 import biowrappers.components.io.hdf5.tasks as hdf5_tasks
 import biowrappers.components.io.vcf.tasks as vcf_tasks
@@ -12,17 +11,24 @@ def create_vcf_db_annotation_workflow(
         target_vcf_file,
         out_file,
         table_name,
+        hdf5_output=True,
         split_size=int(1e4)):
 
-    workflow = Workflow()
+    if hdf5_output:
+        merged_file = mgd.File(out_file)
+
+    else:
+        merged_file = mgd.TempFile('merged.h5')
+
+    workflow = pypeliner.workflow.Workflow()
 
     workflow.transform(
         name='split_vcf',
         ctx={'mem': 2, 'num_retry': 3, 'mem_retry_increment': 2},
         func=vcf_tasks.split_vcf,
         args=(
-            pypeliner.managed.InputFile(target_vcf_file),
-            pypeliner.managed.TempOutputFile('split.vcf', 'split')
+            mgd.InputFile(target_vcf_file),
+            mgd.TempOutputFile('split.vcf', 'split')
         ),
         kwargs={'lines_per_file': split_size}
     )
@@ -33,9 +39,9 @@ def create_vcf_db_annotation_workflow(
         ctx={'mem': 2, 'num_retry': 3, 'mem_retry_increment': 2},
         func=tasks.annotate_db_status,
         args=(
-            pypeliner.managed.InputFile(db_vcf_file),
-            pypeliner.managed.TempInputFile('split.vcf', 'split'),
-            pypeliner.managed.TempOutputFile('annotated.h5', 'split'),
+            mgd.InputFile(db_vcf_file),
+            mgd.TempInputFile('split.vcf', 'split'),
+            mgd.TempOutputFile('annotated.h5', 'split'),
             table_name
         )
     )
@@ -45,9 +51,24 @@ def create_vcf_db_annotation_workflow(
         ctx={'mem': 2, 'num_retry': 3, 'mem_retry_increment': 2},
         func=hdf5_tasks.concatenate_tables,
         args=(
-            pypeliner.managed.TempInputFile('annotated.h5', 'split'),
-            pypeliner.managed.OutputFile(out_file)
+            mgd.TempInputFile('annotated.h5', 'split'),
+            merged_file.as_output()
         )
     )
+
+    if not hdf5_output:
+        workflow.transform(
+            name='convert_to_tsv',
+            ctx={'mem': 2, 'num_retry': 3, 'mem_retry_increment': 2},
+            func=hdf5_tasks.convert_hdf5_to_tsv,
+            args=(
+                merged_file.as_input(),
+                table_name,
+                mgd.OutputFile(out_file),
+            ),
+            kwargs={
+                'compress': True,
+            }
+        )
 
     return workflow
