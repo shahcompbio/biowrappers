ad9210b000be387458f039346adf071cd67da857
Douglas Abrams <abramsd@mskcc.org>
2020-02-13 13:34:31 -0500

    stripped out hd5 and adde csv for vcf annotation

--- biowrappers/components/variant_calling/annotated_db_status/__init__.py
+++ biowrappers/components/variant_calling/annotated_db_status/__init__.py
@@ -1,21 +1,12 @@
 import pypeliner
 import pypeliner.managed as mgd
 
-
 def create_vcf_db_annotation_workflow(
         db_vcf_file,
         target_vcf_file,
         out_file,
-        table_name,
         docker_config={},
-        hdf5_output=True,
         split_size=int(1e4)):
-
-    if hdf5_output:
-        merged_file = mgd.File(out_file)
-
-    else:
-        merged_file = mgd.TempFile('merged.h5')
 
     ctx = dict(mem=2, num_retry=3, mem_retry_increment=2, **docker_config)
 
@@ -40,34 +31,19 @@ def create_vcf_db_annotation_workflow(
         args=(
             db_vcf_file,
             mgd.TempInputFile('split.vcf', 'split'),
-            mgd.TempOutputFile('annotated.h5', 'split'),
-            table_name
+            mgd.TempOutputFile('annotated.csv.gz', 'split',
+                               extensions=['.yaml'])
         )
     )
 
     workflow.transform(
         name='merge_tables',
         ctx=ctx,
-        func='biowrappers.components.io.hdf5.tasks.concatenate_tables',
+        func='single_cell.utils.csvutils.concatenate_csv',
         args=(
-            mgd.TempInputFile('annotated.h5', 'split'),
-            merged_file.as_output()
+            mgd.TempInputFile('annotated.csv.gz', 'split'),
+            mgd.OutputFile(out_file, extensions=['.yaml'])
         )
     )
-
-    if not hdf5_output:
-        workflow.transform(
-            name='convert_to_tsv',
-            ctx=ctx,
-            func='biowrappers.components.io.hdf5.tasks.convert_hdf5_to_tsv',
-            args=(
-                merged_file.as_input(),
-                table_name,
-                mgd.OutputFile(out_file),
-            ),
-            kwargs={
-                'compress': True,
-            }
-        )
 
     return workflow
